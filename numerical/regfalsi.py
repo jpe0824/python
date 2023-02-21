9b546979a2d7b03af23039fb92387a39be1e8e23
jpe0824 <jason.e24@gmail.com>
2023-02-21 15:54:39 -0700

    progress

--- numerical/regfalsi.py
+++ numerical/regfalsi.py
@@ -1,23 +1,73 @@
-test_pass = 0
+import sys
+import numpy as np
 
-def assertEquals(a, b):
-    global test_pass
-    if a == b:
-        print("Pass: ", a)
-        test_pass += 1
+MAX_ITER = 100_000
+
+EPS = sys.float_info.epsilon
+
+def regFalsiResult(xLower, xUpper, func):
+    print('Results for: ',func.__name__)
+    root, flag, fxRoot, iteration = regFalsi(xLower, xUpper, func)
+    if flag == -1:
+        print('No root found.')
         return
-    print(f"Fail:  expected {b}, but got {a}")
+    if flag == 1:
+        print('Max iterations reached')
+        return
+    print('Root found: ', root)
+    print('Function val at root: ', fxRoot)
+    print('No. of iterations: ', iteration)
 
-def testRegFalsi():
-    pass
+def regFalsi(xLower, xUpper, func):
+    if func(xLower) * func(xUpper) < 0:
+        return None, -1, None, None
+    if func(xLower) == 0:
+        return xLower, 0, 0, 0
+    if func(xUpper) == 0:
+        return xUpper, 0, 0, 0
+    root = xLower
+    iteration = 0
+    while iteration < MAX_ITER:
+        prevRoot = root
+        root = (xLower * func(xUpper) - xUpper * func(xLower)) / (func(xUpper) - func(xLower))
 
-def regFalsi():
-    pass
+        if func(root) < EPS:
+            break
+        if func(root) == 0:
+            break
+        if np.isclose(root, prevRoot, rtol = 0, atol=EPS):
+            break
+        elif func(root) * func(xLower) < 0:
+            xUpper = root
+        else:
+            xLower = root
+        iteration += 1
 
+    if iteration == MAX_ITER:
+        return None, 1, None, iteration
+    return root, 0, func(root), iteration
+    # Calculate a new approximated root using the formula of regula falsi in a loop.
+    # Break out the loop and return the proper values if one of the following conditions is
+# met:
+# The func(root)< (machine epsilon)
+# The distance between approximated roots in two consecutive iteration is less a
+# ulp apart at the root location
+# The loop reached the maximum number of iterations. Set maxInt to 100000 in
+# your program.
+
+def func1(x):
+    return (x ** 4) - (6 * x ** 3) + (12 * x ** 2) - (10 * x) + 3
+
+def func2(x):
+    return (x ** 3) - (7 * x ** 2) + (15 * x) - 9
 
 def main():
-    testRegFalsi()
-    print(f"\n{test_pass}/9 tests passed")
+    lower1, lower2 = 1.5, 0
+    upper1, upper2 = 2.5, 1.5
+    regFalsiResult(lower1, upper1, func1)
+    regFalsiResult(lower2, upper2, func1)
+    regFalsiResult(lower1, upper1, func2)
+    regFalsiResult(lower2, upper2, func2)
 
 if __name__ == '__main__':
     main()
