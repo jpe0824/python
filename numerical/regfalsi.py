ab181387f4b53fb2a4dfb87b3cbac2d675e5ae19
jpe0824 <jason.e24@gmail.com>
2023-02-24 21:43:21 -0700

    finished regfalsi

--- numerical/regfalsi.py
+++ numerical/regfalsi.py
@@ -6,20 +6,28 @@ MAX_ITER = 100_000
 EPS = sys.float_info.epsilon
 
 def regFalsiResult(xLower, xUpper, func):
-    print('Results for: ',func.__name__)
+    print('\n')
+    print(f'Results for: {func.__name__}, lower: {xLower}, upper: {xUpper}')
     root, flag, fxRoot, iteration = regFalsi(xLower, xUpper, func)
     if flag == -1:
-        print('No root found.')
+        print('Did not bracket root.')
         return
     if flag == 1:
         print('Max iterations reached')
-        return
     print('Root found: ', root)
     print('Function val at root: ', fxRoot)
     print('No. of iterations: ', iteration)
 
-def regFalsi(xLower, xUpper, func):
-    if func(xLower) * func(xUpper) < 0:
+def regFalsi(xLower, xUpper,func):
+    # Calculate a new approximated root using the formula of regula falsi in a loop.
+    # Break out the loop and return the proper values if one of the following conditions is
+    # met:
+    # The func(root)< (machine epsilon)
+    # The distance between approximated roots in two consecutive iteration is less a
+    # ulp apart at the root location
+    # The loop reached the maximum number of iterations. Set maxInt to 100000 in
+    # your program.
+    if func(xLower) * func(xUpper) >= 0:
         return None, -1, None, None
     if func(xLower) == 0:
         return xLower, 0, 0, 0
@@ -28,32 +36,24 @@ def regFalsi(xLower, xUpper, func):
     root = xLower
     iteration = 0
     while iteration < MAX_ITER:
+        iteration += 1
         prevRoot = root
         root = (xLower * func(xUpper) - xUpper * func(xLower)) / (func(xUpper) - func(xLower))
 
-        if func(root) < EPS:
+        if abs(func(root)) < EPS:
             break
         if func(root) == 0:
             break
-        if np.isclose(root, prevRoot, rtol = 0, atol=EPS):
+        if root == np.nextafter(prevRoot, root):
             break
         elif func(root) * func(xLower) < 0:
             xUpper = root
         else:
             xLower = root
-        iteration += 1
 
     if iteration == MAX_ITER:
-        return None, 1, None, iteration
+        return root, 1, func(root), iteration
     return root, 0, func(root), iteration
-    # Calculate a new approximated root using the formula of regula falsi in a loop.
-    # Break out the loop and return the proper values if one of the following conditions is
-# met:
-# The func(root)< (machine epsilon)
-# The distance between approximated roots in two consecutive iteration is less a
-# ulp apart at the root location
-# The loop reached the maximum number of iterations. Set maxInt to 100000 in
-# your program.
 
 def func1(x):
     return (x ** 4) - (6 * x ** 3) + (12 * x ** 2) - (10 * x) + 3
